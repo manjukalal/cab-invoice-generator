7e49c78c5c7368ee00f6907e1742b6e1638da264
manjukalal <manjukalal1496@gmail.com>
2022-07-15 21:30:29 +0530

    Added: cab invoice generator

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+    private static final int COST_PER_KM = 10;
+    private static final int COST_PER_MIN = 1;
+    private static final int MIN_FARE = 5;
+
+    public double CalculateFare(double distance, int time) {
+
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+
+        if (totalFare < MIN_FARE) {
+
+            return MIN_FARE;
+        }
+        return totalFare;
+    }
+}
