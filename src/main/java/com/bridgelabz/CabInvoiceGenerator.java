703adbea1946d1260d83ba6146f4885824dbbd57
manjukalal <manjukalal1496@gmail.com>
2022-07-18 21:40:24 +0530

    Added: methode to check normal ride and premium ride

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,10 +1,58 @@
 package com.bridgelabz;
+
 import java.util.HashMap;
 import java.util.Map;
+
 public class CabInvoiceGenerator {
+    public static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15;
+    public static final double COST_PER_TIME_PREMIUM = 2;
+    public static final double MINIMUM_FARE_PREMIUM = 20;
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
     private static final int MIN_FARE = 5;
+    private static final double MINIMUM_COST_PER_KILOMETER_NORMAL = 10;
+    private static final int COST_PER_TIME_NORMAL = 1;
+    private static final double MINIMUM_FARE_NORMAL = 5;
+
+    public static double calculateFare(double distance, int time, String type) {
+
+        if (type.equalsIgnoreCase("Normal")) {
+
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_NORMAL + time * COST_PER_TIME_NORMAL;
+
+            return Math.max(totalFare, MINIMUM_FARE_NORMAL);
+
+        } else if (type.equalsIgnoreCase("Premium")) {
+
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+
+            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
+
+        } else {
+
+            System.out.println("Please Enter Proper Customer Type");
+            return 0.0;
+        }
+    }
+
+    public static InvoiceSummary calculateTotalFare(Ride[] rides, String type) {
+
+        double totalFare = 0.0;
+
+        if (type.equalsIgnoreCase("Normal")) {
+
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+            }
+
+        } else if (type.equalsIgnoreCase("Premium")) {
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+            }
+        }
+
+        return new InvoiceSummary(rides.length, totalFare);
+    }
 
     public double calculateFare(double distance, int time) {
 
@@ -22,7 +70,7 @@ public class CabInvoiceGenerator {
         double totalFare = 0.0;
 
         for (Ride ride : rides) {
-            totalFare = calculateFare(ride.getDistance(), ride.getTime());
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
         }
 
         return totalFare;
@@ -40,17 +88,11 @@ public class CabInvoiceGenerator {
 
         Map<Integer, Ride[]> map = new HashMap<>();
 
-        Ride[] rides1 = {new Ride(2.0, 5),
-                new Ride(0.1, 1)
-        };
+        Ride[] rides1 = {new Ride(2.0, 5), new Ride(0.1, 1)};
 
-        Ride[] rides2 = {new Ride(5.0, 10),
-                new Ride(1, 1)
-        };
+        Ride[] rides2 = {new Ride(5.0, 10), new Ride(1, 1)};
 
-        Ride[] rides3 = {new Ride(8.0, 15),
-                new Ride(1, 10)
-        };
+        Ride[] rides3 = {new Ride(8.0, 15), new Ride(1, 10)};
 
         map.put(1, rides1);
         map.put(2, rides2);
@@ -66,5 +108,7 @@ public class CabInvoiceGenerator {
         return null;
     }
 
-
 }
+
+
+
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -28,15 +28,13 @@ public class InvoiceServiceTest {
         Ride[] rides = {new Ride(2.0, 5), new Ride(5.0, 10), new Ride(0.1, 1), new Ride(20, 60)};
         CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
         double totalFare = invoiceService.calculateFareForMultipleRides(rides);
-        Assert.assertEquals(260, totalFare, 0);
+        Assert.assertEquals(350, totalFare, 0);
     }
 
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1),
-        };
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1),};
         CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
         InvoiceSummary invoiceSummary = invoiceService.invoiceSummaryCalculation(rides);
         InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
@@ -46,8 +44,26 @@ public class InvoiceServiceTest {
     @Test
     public void givenUserId_ShouldReturnInvoiceSummary() {
         CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
-        InvoiceSummary invoiceSummary = invoiceService.getInvoice(1);
-        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoice(3);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 115);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+
+    @Test
+    public void givenMultipleRides_WhenNormal_ShouldReturnInvoiceSummary() {
+        String type = "Normal";
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1),};
+        InvoiceSummary invoiceSummary = CabInvoiceGenerator.calculateTotalFare(rides, type);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+
+    @Test
+    public void givenMultipleRides_WhenPremium_ShouldReturnInvoiceSummary() {
+        String type = "Premium";
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1),};
+        InvoiceSummary invoiceSummary = CabInvoiceGenerator.calculateTotalFare(rides, type);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 60.0);
         Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
     }
 
